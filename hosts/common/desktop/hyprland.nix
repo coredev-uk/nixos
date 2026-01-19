4554811961fcdcc0f52e1972e087b7282ed61cc7
Core <core@coredev.uk>
2026-01-19 02:50:06 +0000

    fix: remove redundant var

--- hosts/common/desktop/hyprland.nix
+++ hosts/common/desktop/hyprland.nix
@@ -7,7 +7,6 @@
 let
   theme = import "${self}/lib/theme" { inherit pkgs; };
   hypr-run = pkgs.writeShellScriptBin "hypr-run" ''
-    export XDG_SESSION_TYPE="wayland"
     export XDG_SESSION_DESKTOP="Hyprland"
     export XDG_CURRENT_DESKTOP="Hyprland"
     export GTK_THEME=${theme.gtkTheme.name}
