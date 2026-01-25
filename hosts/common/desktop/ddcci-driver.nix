5c077130eed094d282f9cf5daaffad251bf45e33
Core <core@coredev.uk>
2026-01-25 01:19:51 +0000

    fix(ddcci): add sleep and move stuff around

--- hosts/common/desktop/ddcci-driver.nix
+++ hosts/common/desktop/ddcci-driver.nix
@@ -7,13 +7,13 @@
 
 {
 
-  boot.extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
-  boot.kernelModules = [
-    "i2c-dev"
-    "ddcci_backlight"
-  ];
-
   config = lib.mkIf config.hardware.nvidia.enabled {
+    boot.extraModulePackages = with config.boot.kernelPackages; [ ddcci-driver ];
+    boot.kernelModules = [
+      "i2c-dev"
+      "ddcci_backlight"
+    ];
+
     boot.extraModprobeConfig = ''
       options nvidia NVreg_RegistryDwords="RMUseSwI2c=0x01; RMI2cSpeed=100"
     '';
@@ -21,22 +21,24 @@
     # 3. Auto-Attach Service
     systemd.services.ddcci-nvidia-attach = {
       description = "Attach ddcci driver to all Nvidia I2C buses";
-      after = [ "graphical.target" ];
+      after = [ "display-manager.service" ];
+      requires = [ "display-manager.service" ];
       wantedBy = [ "graphical.target" ];
       serviceConfig = {
         Type = "oneshot";
+        ExecStartPre = "${pkgs.coreutils}/bin/sleep 5";
         ExecStart =
           let
             ddcciSetupScript = pkgs.writeShellScript "ddcci-setup" ''
-
               for i2c_path in /sys/bus/i2c/devices/i2c-*; do
                 if [ -e "$i2c_path/name" ]; then
                   if grep -q "NVIDIA" "$i2c_path/name"; then
-                    echo "Detected Nvidia I2C bus at $i2c_path. Attempting to attach ddcci..."
+                    echo "Detected Nvidia I2C bus at $i2c_path."
                     echo ddcci 0x37 > "$i2c_path/new_device" || true
+                    sleep 1
                   fi
                 fi
-              done
+              done            
             '';
           in
           "${ddcciSetupScript}";
