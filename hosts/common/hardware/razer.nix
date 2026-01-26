ae783ace23591ab9e14660c2b8d20032c5ac6c2b
Core <core@coredev.uk>
2026-01-26 21:50:46 +0000

    fix(openrazer): add user to group and polychromatic

--- hosts/common/hardware/razer.nix
+++ hosts/common/hardware/razer.nix
@@ -1,9 +1,15 @@
-_: {
+{ pkgs, meta, ... }:
+{
   hardware.openrazer = {
     enable = true;
     batteryNotifier = {
       enable = true;
       percentage = 20;
     };
+    users = [ meta.username ];
   };
+
+  environment.systemPackages = with pkgs; [
+    polychromatic
+  ];
 }
