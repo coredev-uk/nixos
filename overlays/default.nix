f3af7c31b34efe37808d22b1670e8588a6e6fcc7
Core <core@coredev.uk>
2026-02-19 01:51:52 +0000

    fix: patch fladder to add a desktop file

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,10 +3,48 @@
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
-  modifications = _final: _prev: {
+  modifications = final: prev: {
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+    fladder =
+      (prev.fladder.override {
+        targetFlutterPlatform = "linux";
+      }).overrideAttrs
+        (oldAttrs: {
+          buildInputs = (oldAttrs.buildInputs or [ ]) ++ final.mpv-unwrapped.buildInputs;
+
+          desktopItems = (oldAttrs.desktopItems or [ ]) ++ [
+            (final.makeDesktopItem {
+              name = "fladder";
+              desktopName = "Fladder";
+              comment = oldAttrs.meta.description;
+              exec = "Fladder";
+              icon = "fladder";
+              terminal = false;
+              type = "Application";
+              categories = [
+                "AudioVideo"
+                "Video"
+                "Network"
+              ];
+              keywords = [
+                "Jellyfin"
+                "Media"
+                "Streaming"
+              ];
+            })
+          ];
+
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
+            final.copyDesktopItems
+          ];
+
+          postInstall = (oldAttrs.postInstall or "") + ''
+            install -Dm644 $out/app/fladder/data/flutter_assets/icons/fladder_icon.svg \
+              $out/share/icons/hicolor/scalable/apps/fladder.svg
+          '';
+        });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
