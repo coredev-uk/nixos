6205252617448b61c2c68bc7a7491e92a5087789
Core <core@coredev.uk>
2026-05-01 01:56:30 +0100

    fix(opencode): remove temp fix

--- home/common/shell/opencode.nix
+++ home/common/shell/opencode.nix
@@ -2,15 +2,6 @@
 {
   programs.opencode = {
     enable = true;
-
-    # Remove override when https://github.com/anomalyco/opencode/issues/23256 is resolved
-    package = inputs.opencode.packages.${meta.system}.opencode.overrideAttrs (old: {
-      preBuild = (old.preBuild or "") + ''
-        substituteInPlace packages/opencode/src/cli/cmd/generate.ts \
-          --replace-fail 'const prettier = await import("prettier")' 'const prettier: any = { format: async (s: string) => s }' \
-          --replace-fail 'const babel = await import("prettier/plugins/babel")' 'const babel = {}' \
-          --replace-fail 'const estree = await import("prettier/plugins/estree")' 'const estree = {}'
-      '';
-    });
+    package = inputs.opencode.packages.${meta.system}.default;
   };
 }
