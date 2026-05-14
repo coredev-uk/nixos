5394e05d045ec8c0cb0466f0820087842143c86c
Core <core@coredev.uk>
2026-05-14 16:31:48 +0100

    chore: remove unused secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,7 @@
-let
-  # Users
-  paul = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQpQFDxvGq+x6sHldr81kFtftS6KFEzbOtoRKKTXFR7";
-in
+# let
+#   # Users
+#   paul = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQpQFDxvGq+x6sHldr81kFtftS6KFEzbOtoRKKTXFR7";
+# in
 {
-  "proton_username.age".publicKeys = [
-    paul
-  ];
-  "proton_password.age".publicKeys = [
-    paul
-  ];
 
 }
