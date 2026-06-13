68814135c48ecb2abbc932e9e856aa24bcc4bfe6
Core <core@coredev.uk>
2026-06-13 01:18:42 +0100

    fix: remove obsolete value

--- home/common/shell/zsh.nix
+++ home/common/shell/zsh.nix
@@ -2,7 +2,6 @@
   lib,
   pkgs,
   meta,
-  config,
   ...
 }:
 {
--- hosts/common/services/vfio.nix
+++ hosts/common/services/vfio.nix
@@ -737,7 +737,7 @@ in
           cgroup_device_acl = [
             "/dev/null", "/dev/full", "/dev/zero",
             "/dev/random", "/dev/urandom",
-            "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
+            "/dev/ptmx", "/dev/kvm",
             "/dev/rtc", "/dev/hpet", "/dev/vfio/vfio",
             "/dev/kvmfr0"
           ]
